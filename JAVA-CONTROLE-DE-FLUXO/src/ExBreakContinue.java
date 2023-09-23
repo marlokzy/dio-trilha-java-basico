7f643e127b900eed9237184ee97ec529ee0df751
marlokzy <marlon.r12325@gmail.com>
2023-09-22 22:40:19 -0300

    add more code´s

---
+++ JAVA-CONTROLE-DE-FLUXO/src/ExBreakContinue.java
@@ -0,0 +1,11 @@
+public class ExBreakContinue {
+    public static void main(String[] args) {
+        for(int x=0; x<=5; x++){
+            if(x == 3)
+                break;
+
+            System.out.println(x);
+        }
+    }
+    
+}
