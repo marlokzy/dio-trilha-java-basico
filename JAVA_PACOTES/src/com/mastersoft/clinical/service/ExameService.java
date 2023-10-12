5d3118dcd7eeba138a95eff8fb98772522e02573
marlokzy <marlon.r12325@gmail.com>
2023-10-12 11:19:29 -0300

    add more code´s

---
+++ JAVA_PACOTES/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+	void salvarExame(Exame exame) { // buscou o Exame do model pq referenciei importando ele logo acima
+		
+	}
+}
